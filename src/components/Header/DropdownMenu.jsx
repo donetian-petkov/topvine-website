5b761100e6b0fba1fc6825c8fa6ec1e5435a1d48
donetian-petkov <donetianpetkov@gmail.com>
2024-10-31 16:08:33 +0200

    update: DropdownMenu -> adding table to the menu for the links

---
+++ src/components/Header/DropdownMenu.jsx
@@ -0,0 +1,59 @@
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+    Container, Box,
+} from '@chakra-ui/react'
+
+export const DropdownMenu = () => {
+
+    return (
+        <Container maxW="100%">
+            <h1>Menu</h1>
+            <TableContainer p={0}>
+                    <Table variant='simple'>
+                        <TableCaption>Imperial to metric conversion factors</TableCaption>
+                        <Thead>
+                            <Tr>
+                                <Th>To convert</Th>
+                                <Th>into</Th>
+                                <Th isNumeric>multiply by</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td>inches</Td>
+                                <Td>millimetres (mm)</Td>
+                                <Td isNumeric>25.4</Td>
+                            </Tr>
+                            <Tr>
+                                <Td>feet</Td>
+                                <Td>centimetres (cm)</Td>
+                                <Td isNumeric>30.48</Td>
+                            </Tr>
+                            <Tr>
+                                <Td>yards</Td>
+                                <Td>metres (m)</Td>
+                                <Td isNumeric>0.91444</Td>
+                            </Tr>
+                        </Tbody>
+                        <Tfoot>
+                            <Tr>
+                                <Th>To convert</Th>
+                                <Th>into</Th>
+                                <Th isNumeric>multiply by</Th>
+                            </Tr>
+                        </Tfoot>
+                    </Table>
+            </TableContainer>
+        </Container>
+
+)
+
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,131 +7,103 @@ import {
     Flex,
     Button,
     HStack,
-    Icon,
-    Container, Box
+    Container,
+    Box
 } from "@chakra-ui/react";
 import logo from "../../assets/img/logo.svg";
-import {MenuIcon} from "../Icons/MenuIcon.jsx";
-import {UserIcon} from "../Icons/UserIcon.jsx";
-import {CartIcon} from "../Icons/CartIcon.jsx";
-import {SearchIcon} from "@chakra-ui/icons";
-import {useState} from "react";
+import { MenuIcon } from "../Icons/MenuIcon.jsx";
+import { UserIcon } from "../Icons/UserIcon.jsx";
+import { CartIcon } from "../Icons/CartIcon.jsx";
+import { CloseIcon, SearchIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+import { DropdownMenu } from "./DropdownMenu.jsx";
 
 export const Header = () => {
-
     const [isDrawerOpen, setDrawerOpen] = useState(false);
-
     const toggleDrawer = () => setDrawerOpen(!isDrawerOpen);
 
+    const commonLinkStyles = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        _hover: {
+            color: "textSecondary",
+            textDecoration: "none"
+        }
+    };
+
     return (
-        <>
-        <Flex as="nav"
-              maxW="100%"
-              bg="accentPrimary"
-              color="backgroundSecondary"
-              fontFamily="body"
-              h="150px"
-              justifyContent={"center"}
-              alignItems={"center"}
-              display={"flex"}
-              gap={"60px"}
-        >
-            <Link href='https://chakra-ui.com' isExternal>
-                <Image
-                    src={`${logo}`}
-                    alt="logo"
-                    w="399px"
-                    h="72px"/>
-            </Link>
-            <InputGroup w="351px" h="44px" color="textSecondary">
-                <Input
-                    type='tel'
-                    placeholder='Search product, model, sku, keyword…'
-                    _placeholder={{color: 'textSecondary'}}
-                    bg="backgroundSecondary"
-                    borderRadius={"8px"}
-                    border="none"
-                />
-                <InputRightElement pointerEvents='none'>
-                    <SearchIcon color='textSecondary'/>
-                </InputRightElement>
-            </InputGroup>
-            <HStack
-                spacing={"60px"}
+        <Container maxW="100%" p="0" m="0">
+            <Flex
+                as="nav"
+                maxW="100%"
+                bg="accentPrimary"
+                color="backgroundSecondary"
+                fontFamily="body"
+                h="150px"
+                justifyContent="center"
+                alignItems="center"
+                gap="60px"
             >
-                <Link
-                    href='/cart'
-                    display={"flex"}
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                    flexDirection={"column"}
-                    _hover={{
-                        color: "textSecondary",
-                        textDecoration: "none"
-                    }}
-                >
-                    <CartIcon
-                        alt="cart"
-                        w="31px"
-                        h="27px"
-                    />
-                    cart
+                <Link href="/">
+                    <Image src={`${logo}`} alt="logo" w="399px" h="72px" />
                 </Link>
-                <Link
-                    href='/login'
-                    display={"flex"}
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                    flexDirection={"column"}
-                    _hover={{
-                        color: "textSecondary",
-                        textDecoration: "none"
-                    }}
-                >
-                    <UserIcon
-                        alt="login"
-                        w="31px"
-                        h="27px"
+
+                <InputGroup w="351px" h="44px" color="textSecondary">
+                    <Input
+                        type="tel"
+                        placeholder="Search product, model, sku, keyword…"
+                        _placeholder={{ color: "textSecondary" }}
+                        bg="backgroundSecondary"
+                        borderRadius="8px"
+                        border="none"
                     />
-                    login
-                </Link>
-                <Button
-                    variant="unstyled"
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    flexDirection="column"
-                    _hover={{
-                        color: "textSecondary",
-                        textDecoration: "none",
-                    }}
-                    onClick={toggleDrawer}
+                    <InputRightElement pointerEvents="none">
+                        <SearchIcon color="textSecondary" />
+                    </InputRightElement>
+                </InputGroup>
+
+                <HStack spacing="60px">
+                    <Link href="/cart" {...commonLinkStyles}>
+                        <CartIcon alt="cart" w="31px" h="27px" />
+                        cart
+                    </Link>
+
+                    <Link href="/login" {...commonLinkStyles}>
+                        <UserIcon alt="login" w="31px" h="27px" />
+                        login
+                    </Link>
+
+                    <Button
+                        variant="unstyled"
+                        {...commonLinkStyles}
+                        onClick={toggleDrawer}
+                    >
+                        {!isDrawerOpen ? (
+                            <MenuIcon alt="menu" w="31px" h="27px" />
+                        ) : (
+                            <CloseIcon />
+                        )}
+                        menu
+                    </Button>
+                </HStack>
+            </Flex>
+
+            {isDrawerOpen && (
+                <Box
+                    position="absolute"
+                    top="150px"  // Adjust to header height
+                    left="0"
+                    width="100%"
+                    bg="#d7dde2"
+                    boxShadow="md"
+                    p={4}
+                    zIndex="999"
                 >
-                    <MenuIcon
-                        alt="menu"
-                        w="31px"
-                        h="27px"
-                    />
-                    menu
-                </Button>
-            </HStack>
-        </Flex>
-    {isDrawerOpen && (
-        <Box
-            position="absolute"
-            top="150px"  // Adjust to header height
-            left="0"
-            width="100%"
-            bg="gray.100"
-            boxShadow="md"
-            p={4}
-            zIndex="999"
-        >
-            <Button w="full" mb={2}>Option 1</Button>
-            <Button w="full" mb={2}>Option 2</Button>
-            <Button w="full">Option 3</Button>
-        </Box>
-    )}
-        </>
-    )
-}
+                    <DropdownMenu />
+                </Box>
+            )}
+        </Container>
+    );
+};
