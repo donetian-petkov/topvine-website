4aefb60c7e13c9d15d09fbd66aac4e5e2e7d365a
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-11-03 07:32:58 +0200

    update: Hero Section -> font and size changes to Text

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,10 +2,13 @@ import {Box, Image, Text} from "@chakra-ui/react";
 
 export const HeroSection = () => {
     return (
-        <Box pos="relative">
+        <Box fontFamily="title" letterSpacing="0.67px" lineHeight="52px">
             <Image src="/img/hero.png"/>
-            <Text pos="absolute" top="198px" left="534px" width="604px" height="124x" color="red" transform="translate(-50%,-50%)">
-                This text is centered on the image
+            <Text pos="absolute" top="198px" left="534px" width="604px" height="124x" color="accentPrimary" fontSize="40" fontWeight="bold" textAlign="right">
+                The Aquatic Professionals Number One Choice
+            </Text>
+            <Text pos="absolute" top="314px" left="675px" width="463px" height="43px" color="red" fontSize="28px">
+                For Strainers, Reducers & Float Valves
             </Text>
         </Box>
     )
