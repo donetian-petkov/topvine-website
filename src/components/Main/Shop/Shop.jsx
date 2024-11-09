5fcde9e6cab61cccb26d0092b1a9ebd25b4334aa
donetian-petkov <donetianpetkov@gmail.com>
2024-11-09 22:03:08 +0200

    update: Shop section -> categories

--- src/components/Main/Shop/Shop.jsx
+++ src/components/Main/Shop/Shop.jsx
@@ -6,23 +6,26 @@ function Product() {
 
 export const Shop = () => {
     return (
-       <Container>
-           <Box textAlign="center" m="52px">
-               <Heading fontSize="48px" p="22px" >Shop Fluidtrol</Heading>
-               <Button bg="accentSecondary" color="backgroundMain" borderRadius="22px"> View All Products</Button>
-           </Box>
-           <Box>
-               <Text></Text>
-               <Product></Product>
-               <Product></Product>
-               <Product></Product>
-           </Box>
-           <Box>
-               <Text></Text>
-               <Product></Product>
-               <Product></Product>
-               <Product></Product>
-           </Box>
-       </Container>
+        <>
+            <Container>
+                <Container textAlign="center" m="44px">
+                    <Heading fontSize="48px" p="22px" color="accentPrimary">Shop Fluidtrol</Heading>
+                    <Button bg="accentSecondary" color="backgroundMain" borderRadius="22px" w="330px"> View All
+                        Products</Button>
+                </Container>
+            </Container>
+            <Text bg="acentPrimary" w="100%" textAlign="left">Product Categories</Text>
+            <Container>
+                <Product></Product>
+                <Product></Product>
+                <Product></Product>
+            </Container>
+            <Text>Product Categories</Text>
+            <Container>
+                <Product></Product>
+                <Product></Product>
+                <Product></Product>
+            </Container>
+        </>
     )
 }
