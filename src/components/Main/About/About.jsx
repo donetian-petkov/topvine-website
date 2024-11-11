aafef9514c2a0d8087ef24de1d1cd3c4d5cb2022
donetian-petkov <donetianpetkov@gmail.com>
2024-11-11 23:39:50 +0200

    feature: About

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -1,7 +1,34 @@
+import {Container, Heading, Text, Image, HStack, VStack} from "@chakra-ui/react";
+
 export const About = () => {
     return (
-        <section>
-        <h2>About</h2>
-        </section>
+        <Container w="1200px" h="760px">
+            <Heading>
+                Why Trust Fluidtrol?
+            </Heading>
+            <HStack>
+                <VStack textAlign="left" w="330px">
+                    <Text>ISO 9001:2008 Certified </Text>
+                    <Text> Our quality system certification ensures the highest manufacturing standards. </Text>
+                    <Text>Expert Customization </Text>
+                    <Text>We work with your design engineers to meet your exact requirements. </Text>
+                    <Text>99% On-Time Delivery </Text>
+                    <Text> We consider timely delivery a crucial part of our service to our customers. </Text>
+                    <Text> Warehousing Inventory </Text>
+                    <Text> is managed on-site to reduce lead times and keep costs low.</Text>
+                </VStack>
+                <VStack textAlign="left" w="330px">
+                    <Text>ISO 9001:2008 Certified </Text>
+                    <Text> Our quality system certification ensures the highest manufacturing standards. </Text>
+                    <Text>Expert Customization </Text>
+                    <Text>We work with your design engineers to meet your exact requirements. </Text>
+                    <Text>99% On-Time Delivery </Text>
+                    <Text> We consider timely delivery a crucial part of our service to our customers. </Text>
+                    <Text> Warehousing Inventory </Text>
+                    <Text> is managed on-site to reduce lead times and keep costs low.</Text>
+                </VStack>
+                <Image src="/img/about.png"/>
+            </HStack>
+        </Container>
     )
 }
