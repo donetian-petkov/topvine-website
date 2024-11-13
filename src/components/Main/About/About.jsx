db6ca3bf4eb0e8a10812081ca08e47b6ff9864a9
donetian-petkov <donetianpetkov@gmail.com>
2024-11-13 08:12:43 +0200

    update: About section -> Heading and Paragraph

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -2,30 +2,30 @@ import {Container, Heading, Text, Image, HStack, VStack} from "@chakra-ui/react"
 
 export const About = () => {
     return (
-        <Container w="1200px" h="760px">
+        <Container w="1200px" h="760px" m='0' alignItems="center" justifyContent="space-between">
             <Heading>
                 Why Trust Fluidtrol?
             </Heading>
-            <HStack>
-                <VStack textAlign="left" w="330px">
-                    <Text>ISO 9001:2008 Certified </Text>
-                    <Text> Our quality system certification ensures the highest manufacturing standards. </Text>
-                    <Text>Expert Customization </Text>
-                    <Text>We work with your design engineers to meet your exact requirements. </Text>
-                    <Text>99% On-Time Delivery </Text>
-                    <Text> We consider timely delivery a crucial part of our service to our customers. </Text>
-                    <Text> Warehousing Inventory </Text>
-                    <Text> is managed on-site to reduce lead times and keep costs low.</Text>
+            <HStack textAlign="left" fontSize="16px" m="10px">
+                <VStack w="330px">
+                    <Text w="309px" fontWeight="bold">ISO 9001:2008 Certified </Text>
+                    <Text  w="309px"> Our quality system certification ensures the highest manufacturing standards. </Text>
+                    <Text  w="309px" fontWeight="bold">Expert Customization </Text>
+                    <Text  w="309px">We work with your design engineers to meet your exact requirements. </Text>
+                    <Text fontWeight="bold"  w="309px">99% On-Time Delivery </Text>
+                    <Text  w="309px"> We consider timely delivery a crucial part of our service to our customers. </Text>
+                    <Text fontWeight="bold"  w="309px"> Warehousing Inventory </Text>
+                    <Text  w="309px"> is managed on-site to reduce lead times and keep costs low.</Text>
                 </VStack>
-                <VStack textAlign="left" w="330px">
-                    <Text>ISO 9001:2008 Certified </Text>
-                    <Text> Our quality system certification ensures the highest manufacturing standards. </Text>
-                    <Text>Expert Customization </Text>
-                    <Text>We work with your design engineers to meet your exact requirements. </Text>
-                    <Text>99% On-Time Delivery </Text>
-                    <Text> We consider timely delivery a crucial part of our service to our customers. </Text>
-                    <Text> Warehousing Inventory </Text>
-                    <Text> is managed on-site to reduce lead times and keep costs low.</Text>
+                <VStack w="330px">
+                    <Text w="309px" fontWeight="bold">ISO 9001:2008 Certified </Text>
+                    <Text  w="309px"> Our quality system certification ensures the highest manufacturing standards. </Text>
+                    <Text  w="309px" fontWeight="bold">Expert Customization </Text>
+                    <Text  w="309px">We work with your design engineers to meet your exact requirements. </Text>
+                    <Text fontWeight="bold"  w="309px">99% On-Time Delivery </Text>
+                    <Text  w="309px"> We consider timely delivery a crucial part of our service to our customers. </Text>
+                    <Text fontWeight="bold"  w="309px"> Warehousing Inventory </Text>
+                    <Text  w="309px"> is managed on-site to reduce lead times and keep costs low.</Text>
                 </VStack>
                 <Image src="/img/about.png"/>
             </HStack>
