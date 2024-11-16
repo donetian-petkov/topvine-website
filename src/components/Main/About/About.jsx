44e4becdeec0c51a1995d447dd7a886923023ffb
donetian-petkov <donetianpetkov@gmail.com>
2024-11-16 13:48:12 +0200

    update: About section -> Heading and Paragraph

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -29,6 +29,29 @@ export const About = () => {
                 </VStack>
                 <Image src="/img/about.png"/>
             </HStack>
+            <HStack textAlign="left" fontSize="16px" m="10px">
+                <VStack w="330px">
+                    <Text w="309px" fontWeight="bold">ISO 9001:2008 Certified </Text>
+                    <Text  w="309px"> Our quality system certification ensures the highest manufacturing standards. </Text>
+                    <Text  w="309px" fontWeight="bold">Expert Customization </Text>
+                    <Text  w="309px">We work with your design engineers to meet your exact requirements. </Text>
+                    <Text fontWeight="bold"  w="309px">99% On-Time Delivery </Text>
+                    <Text  w="309px"> We consider timely delivery a crucial part of our service to our customers. </Text>
+                    <Text fontWeight="bold"  w="309px"> Warehousing Inventory </Text>
+                    <Text  w="309px"> is managed on-site to reduce lead times and keep costs low.</Text>
+                </VStack>
+                <VStack w="330px">
+                    <Text w="309px" fontWeight="bold">ISO 9001:2008 Certified </Text>
+                    <Text  w="309px"> Our quality system certification ensures the highest manufacturing standards. </Text>
+                    <Text  w="309px" fontWeight="bold">Expert Customization </Text>
+                    <Text  w="309px">We work with your design engineers to meet your exact requirements. </Text>
+                    <Text fontWeight="bold"  w="309px">99% On-Time Delivery </Text>
+                    <Text  w="309px"> We consider timely delivery a crucial part of our service to our customers. </Text>
+                    <Text fontWeight="bold"  w="309px"> Warehousing Inventory </Text>
+                    <Text  w="309px"> is managed on-site to reduce lead times and keep costs low.</Text>
+                </VStack>
+                <Image src="/img/about.png"/>
+            </HStack>
         </Container>
     )
 }
