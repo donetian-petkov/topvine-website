b94d4af0f84eb8a2615659df26b21057f49148bb
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-11-16 16:27:29 +0200

    feature: Footer update: Footer - > Social Media Icons update: Footer - > Styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,17 @@
+import {Image, Heading, HStack, Text, VStack} from "@chakra-ui/react";
+
 export const Footer = () => {
     return (
-        <footer>
-        <p>&copy; 2021</p>
-        </footer>
+        <HStack bg="accentPrimary" p="50px" color="backgroundMain">
+            <Text color="backgroundPrimary">© 2023 Fluidtrol Process Technologies, Inc. All Rights Reserved</Text>
+            <VStack>
+                <Text>For Customer Service Call</Text>
+                <Heading>888-551-0511</Heading>
+                <Text>or send an email to: fpt@fluidtrol.com</Text>
+            </VStack>
+            <HStack><Image src="/img/linkedin.png"/>
+                <Image src="/img/twitter.png"/>
+                <Image src="/img/facebook.png"/></HStack>
+        </HStack>
     )
 }
